06f92c58f0bf9a563692ab1036487c32148cf88a
gustnxodjs <gustnxodjs@snu.ac.kr>
2015-04-06 20:27:05 +0900

    haha

---
+++ app/assets/javascripts/welcome.coffee
@@ -0,0 +1,2 @@
+@HoorayFunction = () -> alert
+"Hooray!"
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   get 'welcome/index'
   root 'welcome#index'
-  resources :articles 
+  resources :articles
+  post 'articles/count' 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
